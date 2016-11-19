11d23379cb90a7f2f38ca1d56d76674e23e7b5fe
dgarin <dantegarin@gmail.com>
2016-11-18 22:10:53 -0300

    Certamen 3 P1 Dante Garin 201030007-2

--- app/src/main/java/cl/telematica/android/certamen3/activities/MyFavsActivity.java
+++ app/src/main/java/cl/telematica/android/certamen3/activities/MyFavsActivity.java
@@ -1,6 +1,7 @@
 package cl.telematica.android.certamen3.activities;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import cl.telematica.android.certamen3.R;
 import cl.telematica.android.certamen3.models.Feed;
@@ -16,14 +17,19 @@ public class MyFavsActivity extends MainActivity {
 
     private Realm realm;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favs);
 
+        realm = Realm.getDefaultInstance();
+
+        //Obtengo datos de base de datos
         RealmResults<Feed> result2 = realm.where(Feed.class)
                 .findAll();
-
+        System.out.println(result2);
+        
 
     }
 
